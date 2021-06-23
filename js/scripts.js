8fbb1bb28dd8678ce760525bb77cec2db19fd46c
Doris24 <33037450+Doris24@users.noreply.github.com>
2021-06-23 10:35:06 +0200

    Created Pokemon List Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello world');
+// List of Pokemons
+let pokemonList = [];
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+pokemonList[0] = {
+  name: 'Bulbasaur',
+  height: 7,
+  types: ['grass', 'poison']};
+pokemonList[1] = {
+  name: 'Ivysaur',
+  height: 10,
+  types: ['grass', 'poison']};
+pokemonList[2] = {
+  name: 'Venusaur',
+  height: 20,
+  types: ['grass', 'poison']};
+pokemonList[3] = {
+  name: 'Charmander',
+  height: 6,
+  types: ['fire']};
